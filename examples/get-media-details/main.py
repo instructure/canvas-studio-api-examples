79e34eb2a6e092050d891f8fcdbef0707c4961c5
Andras Nemeth <andras.nemeth@instructure.com>
2022-06-14 11:50:09 +0200

    Refactored get_media_details into MediaDetailer

--- examples/get-media-details/main.py
+++ examples/get-media-details/main.py
@@ -9,14 +9,14 @@ def main():
             (["media_id"], {"type": int, "help": "the media to get details about"}),
         ]
     )
-    media_id = args.media_id
 
-    public_api_client = PublicAPIClient(args.subdomain)
-    media = get_media_details(public_api_client, media_id)
-    captions = get_media_details(public_api_client, media_id, "caption_files")
-    courses = get_media_details(public_api_client, media_id, "courses")
-    tags = get_media_details(public_api_client, media_id, "tags")
-    users = get_media_details(public_api_client, media_id, "users", "user_permissions")
+    media_detailer = MediaDetailer(args.subdomain, args.media_id)
+    media = media_detailer.get()
+
+    captions = media_detailer.get("caption_files")
+    courses = media_detailer.get("courses")
+    tags = media_detailer.get("tags")
+    users = media_detailer.get("users", "user_permissions")
 
     response = get_subdict(
         media,
@@ -49,20 +49,23 @@ def main():
     )
 
 
-def get_media_details(
-    public_api_client, media_id, detail_url=None, detail_attribute=None
-):
-    if detail_url:
-        url = f"media/{media_id}/{detail_url}"
-        if detail_attribute is None:
-            detail_attribute = detail_url
-    else:
-        url = f"media/{media_id}"
-        detail_attribute = "media"
-    response = public_api_client.request("get", url)
-    if response.status_code != 200:
-        raise Exception(f"Could not get details for {url}: {response.text}")
-    return response.json()[detail_attribute]
+class MediaDetailer:
+    def __init__(self, subdomain, media_id):
+        self.public_api_client = PublicAPIClient(subdomain)
+        self.media_id = media_id
+
+    def get(self, detail_url=None, detail_attribute=None):
+        if detail_url:
+            url = f"media/{self.media_id}/{detail_url}"
+            if detail_attribute is None:
+                detail_attribute = detail_url
+        else:
+            url = f"media/{self.media_id}"
+            detail_attribute = "media"
+        response = self.public_api_client.request("get", url)
+        if response.status_code != 200:
+            raise Exception(f"Could not get details for {url}: {response.text}")
+        return response.json()[detail_attribute]
 
 
 def get_subdict(dict_, keys):
